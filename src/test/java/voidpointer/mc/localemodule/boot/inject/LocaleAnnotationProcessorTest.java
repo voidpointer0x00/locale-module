65415af070f13c09ee4166cf7e63862d6a6f50d3
Vasiliy <void.pointer@ya.ru>
2023-05-17 08:30:23 +0500

    Refactoring: use data provider

--- src/test/java/voidpointer/mc/localemodule/boot/inject/LocaleAnnotationProcessorTest.java
+++ src/test/java/voidpointer/mc/localemodule/boot/inject/LocaleAnnotationProcessorTest.java
@@ -8,7 +8,9 @@
 
 package voidpointer.mc.localemodule.boot.inject;
 
+import lombok.Getter;
 import org.mockito.Mockito;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import voidpointer.mc.localemodule.Locale;
 import voidpointer.mc.localemodule.Log;
@@ -18,19 +20,21 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertSame;
 
-@Test
+@Test(groups="inject")
 public class LocaleAnnotationProcessorTest {
 
-    @Test(description = "Tests whether annotation processor injects the right locales")
-    public void testInjects() {
-        class NamedTestTarget {
+    interface DoubleLocaleHolder { Locale getFirst(); Locale getSecond(); }
+
+    @DataProvider
+    public static Object[][] provideInjects() {
+        @Getter class NamedTestTarget implements DoubleLocaleHolder {
             static final String JUST_LOCALE_PATH = "just-locale";
             static final String PLUGIN_LOCALE_PATH = "plugin-locale";
-            @InjectLocale(filenamePattern = JUST_LOCALE_PATH) Locale justLocale;
-            @InjectLocale(filenamePattern = PLUGIN_LOCALE_PATH) PluginLocale pluginLocale;
+            @InjectLocale(filenamePattern = PLUGIN_LOCALE_PATH) PluginLocale first;
+            @InjectLocale(filenamePattern = JUST_LOCALE_PATH) Locale second;
         }
-        class SequentialTestTarget {
-            @InjectLocale Locale firstLocale; @InjectLocale PluginLocale secondLocale;
+        @Getter class SequentialTestTarget implements DoubleLocaleHolder {
+            @InjectLocale Locale first; @InjectLocale PluginLocale second;
         }
         var named = new NamedTestTarget();
         var sequentialTarget = new SequentialTestTarget();
@@ -42,22 +46,29 @@ public class LocaleAnnotationProcessorTest {
         when(justLocale.logger()).thenReturn(log);
         when(pluginLocale.logger()).thenReturn(log);
 
-        var pluginLocaleFactory = Mockito.mock(PluginLocaleFactory.class);
+        var localeFactory = Mockito.mock(PluginLocaleFactory.class);
         /* test if injected according to filename pattern order */
-        when(pluginLocaleFactory.locale(eq(NamedTestTarget.PLUGIN_LOCALE_PATH))).thenReturn(pluginLocale);
-        when(pluginLocaleFactory.locale(eq(NamedTestTarget.JUST_LOCALE_PATH))).thenReturn(justLocale);
-        LocaleAnnotationProcessor.process(named, pluginLocaleFactory);
-        assertSame(named.justLocale, justLocale);
-        assertSame(named.pluginLocale, pluginLocale);
-        /* just locale first, plugin next */
-        when(pluginLocaleFactory.locale(InjectLocale.FILENAME_PATTERN)).thenReturn(justLocale, pluginLocale);
-        LocaleAnnotationProcessor.process(sequentialTarget, pluginLocaleFactory);
-        assertSame(sequentialTarget.firstLocale, justLocale);
-        assertSame(sequentialTarget.secondLocale, pluginLocale);
-        /* reverse order */
-        when(pluginLocaleFactory.locale(InjectLocale.FILENAME_PATTERN)).thenReturn(pluginLocale, justLocale);
-        LocaleAnnotationProcessor.process(sequentialTarget, pluginLocaleFactory);
-        assertSame(sequentialTarget.firstLocale, pluginLocale);
-        assertSame(sequentialTarget.secondLocale, justLocale);
+        when(localeFactory.locale(eq(NamedTestTarget.PLUGIN_LOCALE_PATH))).thenReturn(pluginLocale);
+        when(localeFactory.locale(eq(NamedTestTarget.JUST_LOCALE_PATH))).thenReturn(justLocale);
+        when(localeFactory.locale(InjectLocale.FILENAME_PATTERN)).thenReturn(pluginLocale, justLocale,
+                justLocale, pluginLocale);
+        return new Object[][] {
+                {named, localeFactory, pluginLocale, justLocale},
+                {sequentialTarget, localeFactory, pluginLocale, justLocale},
+                {sequentialTarget, localeFactory, justLocale, pluginLocale}
+        };
+    }
+
+    @Test(dataProvider = "provideInjects", description="Tests whether annotation processor injects the right locales")
+    public void testInjects(DoubleLocaleHolder doubleLocaleHolder, PluginLocaleFactory pluginLocaleFactory,
+                            Locale expectedFirst, Locale expectedSecond) {
+        LocaleAnnotationProcessor.process(doubleLocaleHolder, pluginLocaleFactory);
+        assertSame(doubleLocaleHolder.getFirst(), expectedFirst);
+        assertSame(doubleLocaleHolder.getSecond(), expectedSecond);
+    }
+
+    @Test(dependsOnMethods="testInjects", description="Tests whether annotation processor injects keys")
+    public void testInjectsKeys() {
+
     }
 }
